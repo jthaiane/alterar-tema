b1c4c0867d6320ed18fd492be90fcb7ee668fd80
jthaiane <163008711+jthaiane@users.noreply.github.com>
2025-04-25 09:33:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function mudatema(tema) {
+
+    if (tema==='ice') {
+        document.body.classList.remove('fire');
+        document.body.classList.add('ice');
+        
+}
+else{
+    document.body.classList.remove('ice');
+    documento.body.classList.add('fire');
+}
+   } 
+
+    
